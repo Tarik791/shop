f95734ff7b1a96f2160b46e870b5723df381ff3c
Tarik791 <terzotarik@gmail.com>
2025-04-16 14:55:14 +0200

    Modify models

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    //
+    protected $primaryKey = 'uuid';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = ['uuid', 'url'];
 }
